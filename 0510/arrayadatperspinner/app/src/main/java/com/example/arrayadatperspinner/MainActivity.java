e46219c85f68e005477abecc4101a67ebad68c78
Linrbaba <linrbaba@gmail.com>
2024-05-10 14:53:26 +0800

    20240510

---
+++ 0510/arrayadatperspinner/app/src/main/java/com/example/arrayadatperspinner/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.arrayadatperspinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txv = (TextView) findViewById(R.id.order);
+        temp = (Spinner)findViewById(R.id.order);
+        drink = (Spinner) findViewById(R.id.drink);
+
+    }
+}
